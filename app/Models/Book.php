3782089f5c67943430301104af5afd26df0e6a72
Mostafa Hassan <mostafahassan999888@gmail.com>
2024-05-04 02:33:36 +0300

    Add relationship between reading intervals and books, users

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
@@ -14,5 +15,10 @@ class Book extends Model
     public function scopeMostRead(Builder $query): Builder
     {
         return $query->orderBy('number_of_read_pages','desc');
+    }
+
+    public function readingIntervals(): HasMany
+    {
+        return $this->hasMany(User::class);
     }
 }
