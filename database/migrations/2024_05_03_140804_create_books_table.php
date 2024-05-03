13a5bc9168e8afc5770df7d6079d8a6b71c47c5f
Mostafa Hassan <mostafahassan999888@gmail.com>
2024-05-03 17:12:38 +0300

    Add book model

---
+++ database/migrations/2024_05_03_140804_create_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->unsignedInteger('number_of_pages');
+            $table->unsignedInteger('number_of_read_pages')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
