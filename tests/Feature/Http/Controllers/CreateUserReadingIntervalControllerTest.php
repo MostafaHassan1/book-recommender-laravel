d427af84258dac8f66697947b705610c3e47c07c
Mostafa Hassan <mostafahassan999888@gmail.com>
2024-05-04 02:56:55 +0300

    Add todo tests for create user reading interval

---
+++ tests/Feature/Http/Controllers/CreateUserReadingIntervalControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+todo('can create reading interval for a user');
+todo('throws validation error if user not found');
+todo('throws validation error if book not found');
+todo('throws validation error if start page is bigger than the book\'s number_of_pages');
+todo('throws validation error if start page is less than 1');
+todo('throws validation error if end page is bigger than the book\'s number_of_pages');
+todo('throws validation error if end page is less than start page');
